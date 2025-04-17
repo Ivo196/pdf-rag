7839ac3c6adc20910bc4ee73860e43ceb8ccf49e
Ivo106 <ivo.tonioni@gmail.com>
2025-04-17 12:28:02 +0200

    add Pinecone

--- app/app.py
+++ app/app.py
@@ -10,13 +10,21 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
+
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
+
+pc = Pinecone()
+index = pc.Index('langchain')
 
 def get_document_from_web(url):
     loader = WebBaseLoader(url)
     docs = loader.load()
+    return docs
+
+def splitter(docs):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=200,
         chunk_overlap=20
@@ -28,7 +36,11 @@ def create_db(docs):
     embeddings = OpenAIEmbeddings(
         model="text-embedding-3-small"
     )
-    vectorStore = FAISS.from_documents(docs, embeddings)
+    vectorStore = PineconeVectorStore(
+        embedding=embeddings,
+        index=index
+    )
+    vectorStore.add_documents(docs)
     return vectorStore
 
 def create_chain(vectorStore):
@@ -60,12 +72,13 @@ def create_chain(vectorStore):
     
 
 docs = get_document_from_web('https://www.youtube.com/watch?v=-Ueh5XBpcoY&t=682s')
-vectorStore = create_db(docs)
+split_docs = splitter(docs)
+vectorStore = create_db(split_docs)
 chain = create_chain(vectorStore)
 
 
 response = chain.invoke({
-    'input': "What is that?"
+    'input': "What is the video talking about?"
 })
 print(response['answer'])
 
